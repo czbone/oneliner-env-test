23767b4096d1e7901dbbe9deff25a00ac4a71bef
naoki hirata <naoki@magic3.org>
2019-03-19 15:22:43 +0900

    スクリプト更新。

--- test/build_test.sh
+++ test/build_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/czbone/oneliner-env-test/master/script/build_test.sh | bash /dev/stdin -test
+curl -s https://raw.githubusercontent.com/czbone/oneliner-env-test/master/script/build_test.sh | bash /dev/stdin -test
